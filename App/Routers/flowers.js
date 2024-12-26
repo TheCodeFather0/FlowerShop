504a1fb43b5f82378b23c7525e51ae8ad75de92f
TheCodeFather0 <raminmammadov056@gmail.com>
2024-12-26 17:02:49 +0400

    added not found errors

--- App/Routers/flowers.js
+++ App/Routers/flowers.js
@@ -1,23 +1,52 @@
 import { Router } from "express";
 import { FlowerModel } from "../Schema/FlowerSchema.js";
+import { getServerError } from "../Utils/Errors.js";
 
 export const FlowersRouters = new Router();
 
+//get
 FlowersRouters.get("/", (req, res) => {
   FlowerModel.find().then((data) => {
-    res.status(200).json(data);
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({
+        message: "Flower not found",
+      });
+    }
   });
 });
 
+FlowersRouters.get("/:id", (req, res) => {
+  const { id } = req.params;
+  FlowerModel.findById(id)
+    .then((data) => {
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res.status(404).json({
+          message: "Flower not found",
+        });
+      }
+    })
+    .catch((err) => getServerError(res, err));
+});
+
+//post
 FlowersRouters.post("/", async (req, res) => {
   FlowerModel.create(req.body)
     .then(() => {
       res.status(201).json({ message: "Flower created successfully" });
     })
-    .catch((err) => {
-      res.status(500).json({
-        message: "Server error, failed to create flower",
-        error: err.message,
-      });
-    });
+    .catch((err) => getServerError(res, err));
+});
+
+// delete
+FlowersRouters.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  FlowerModel.findByIdAndDelete(id)
+    .then(() => {
+      res.status(200).json({ message: "Flower deleted successfully" });
+    })
+    .catch((err) => getServerError(res, err));
 });
