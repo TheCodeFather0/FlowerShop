c76003d858a49453a1f528ad20668ade6a3544ff
TheCodeFather0 <raminmammadov056@gmail.com>
2024-12-26 16:45:20 +0400

    getAll Flowers and create flower completed

--- App/Routers/flowers.js
+++ App/Routers/flowers.js
@@ -1,7 +1,23 @@
 import { Router } from "express";
+import { FlowerModel } from "../Schema/FlowerSchema.js";
 
 export const FlowersRouters = new Router();
 
 FlowersRouters.get("/", (req, res) => {
-  res.send([]);
+  FlowerModel.find().then((data) => {
+    res.status(200).json(data);
+  });
+});
+
+FlowersRouters.post("/", async (req, res) => {
+  FlowerModel.create(req.body)
+    .then(() => {
+      res.status(201).json({ message: "Flower created successfully" });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Server error, failed to create flower",
+        error: err.message,
+      });
+    });
 });
